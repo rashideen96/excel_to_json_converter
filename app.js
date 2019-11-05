daea0b55a9ca9e84c73e0b8f8427a0af8bef3a29
myruldeen <xyberworm@yahoo.com>
2019-11-05 02:26:48 -0500

    convert to async await

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const multer = require('multer');
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const file_routes = require('./routes/file');
 
 const app = express();
@@ -21,12 +22,7 @@ const filter = (req, file, cb) => {
 app.use(multer({ storage: storage, fileFilter: filter }).single('file'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-	next();
-});
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(file_routes);
 
--- controller/FileController.js
+++ controller/FileController.js
@@ -32,35 +32,56 @@ const remove_file = (file) => {
 	});
 };
 
-exports.upload = (req, res, next) => {
+exports.upload = async (req, res, next) => {
 	if (!req.file) {
 		res.send('File is required');
 	}
 
-	convert_excel_to_json(req.file.path)
-		.then((result) => {
-			res.status(200).json({ data: result });
-			return json_file.writeFile('./json/data.json', result, { spaces: 2 });
-		})
-		.then((result) => {
-			console.log(result);
-			return remove_file(req.file.path);
-		})
-		.then((result) => {
-			console.log(result);
-		})
-		.catch((err) => {
-			console.log(err);
-		});
+	try {
+		const object_data = await convert_excel_to_json(req.file.path);
+		await json_file.writeFile('./json/data.json', object_data, { spaces: 2 });
+		const read_file = await json_file.readFile('./json/data.json');
+
+		res.status(200).json({ data: read_file });
+		const rm_res = await remove_file(req.file.path);
+		console.log(rm_res);
+	} catch (err) {
+		console.log(err.message);
+	}
+
+	
+
+	// convert_excel_to_json(req.file.path)
+	// 	.then((result) => {
+			
+	// 		return json_file.writeFile('./json/data.json', result, { spaces: 2 });
+	// 	})
+	// 	.then((result) => {
+	// 		console.log(result);
+	// 		return remove_file(req.file.path);
+	// 	})
+	// 	.then((result) => {
+	// 		console.log(result);
+	// 	})
+	// 	.catch((err) => {
+	// 		console.log(err);
+	// 	});
 };
 
 exports.index = (req, res, next) => {
 	res.sendFile(path.join(__dirname, '../views', 'index.html'));
 };
 
-exports.getData = (req, res, next) => {
-	json_file.readFile('./json/data.json', (err, data) => {
-		if (err) throw err;
-		res.status(200).json({ data: data });
-	});
+exports.getData = async (req, res, next) => {
+
+	try {
+		const result = await json_file.readFile('./json/data.json');
+		res.status(200).json({ data: result });
+	} catch (err) {
+		console.log(err)
+	}
+	// json_file.readFile('./json/data.json', (err, data) => {
+	// 	if (err) throw err;
+	// 	res.status(200).json({ data: data });
+	// });
 };
